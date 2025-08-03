ae1a926c8b4b3cc6fcd00ec74403727a991879a9
fupingyezi <3457432742@qq.com>
2025-08-03 15:46:02 +0800

    Site updated: 2025-08-03 15:45:58

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1754139713852";
+const VERSION = "1754207149364";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
